0a8e1bec8b2bfee3b84ac4d63577fcb6665bd6e4
sampeeter4 <sampeter9977@gmail.com>
2025-05-03 22:41:30 +0530

    Update App.js

--- blooddonation/src/App.js
+++ blooddonation/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom'; 
 import { ThemeProvider } from '@mui/material/styles';
 import { AuthProvider } from './contexts/AuthContext';
 import AppRoutes from './AppRoutes';
@@ -9,7 +9,7 @@ import './assets/styles/App.css';
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Router>
+      <Router basename="/blood-donation"> 
         <AuthProvider>
           <div className="app">
             <AppRoutes />
